6c13394ca484beb067fd80db673481050cea5142
majohorta04 <2220211028@estudiantesunibague.edu.co>
2023-06-14 19:45:40 -0500

    Se agrega el proyecto

---
+++ src/app/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-app.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCm0ONFyDLcnIkua4fM2V4OPUjL9r8FRJs",
+  authDomain: "fir-app-majo.firebaseapp.com",
+  projectId: "fir-app-majo",
+  storageBucket: "fir-app-majo.appspot.com",
+  messagingSenderId: "213636586536",
+  appId: "1:213636586536:web:37f15b8c4c607cda2ae91d"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
